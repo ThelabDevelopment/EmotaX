753f50e419b40fbd6843a71bf7a7745c4b9e0326
RimakiTaema <txwfyxa@gmail.com>
2025-10-01 16:06:51 +0700

    new stuff and use example res from pnx example

--- src/main/java/net/rimaki/emotax/gui/EmotaXMeuA.java
+++ src/main/java/net/rimaki/emotax/gui/EmotaXMeuA.java
@@ -5,6 +5,6 @@ import cn.nukkit.form.window.SimpleForm;
 public class EmotaXMeuA extends SimpleForm {
     public EmotaXMeuA() {
         super("EmotaX Menu");
-        SimpleForm content1 = new SimpleForm("ssss", "aaa")
+        SimpleForm content1 = new SimpleForm("ssss", "aaa");
     }
 }
